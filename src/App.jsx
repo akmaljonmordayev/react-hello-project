1019ad8740105cf109183c9f51d98912c4bb4988
akmaljonmordayev <akmaljonmordayev@gmail.com>
2025-01-05 15:05:33 +0500

    h6 tag added

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,12 @@ function App() {
           <h1>hello uzbeksitan</h1> 
           <h2>hello tashkent</h2>
           <a href="#">mars space</a>
+          <h6>Lorem ipsum dolor sit.</h6>
+          <h6>Lorem ipsum dolor sit.</h6>
+          <h6>Lorem ipsum dolor sit.</h6>
+          <h6>Lorem ipsum dolor sit.</h6>
+          <h6>Lorem ipsum dolor sit.</h6>
+          <h6>Lorem ipsum dolor sit.</h6>
      </div>
     </>
   )
